f9002a85ed8680d93737365c3405f00b54a1a979
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-05-16 13:14:20 +0000

    Created base Spring Project. Closes #1

---
+++ src/main/java/com/alexquazar/bmspringjokesapp/BmSpringJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.alexquazar.bmspringjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmSpringJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmSpringJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexquazar/bmspringjokesapp/BmSpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexquazar.bmspringjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmSpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
