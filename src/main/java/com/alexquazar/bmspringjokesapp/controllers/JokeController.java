f8c9247b574b90222905e71c7fc63d769a0fbc1f
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-06-19 16:03:32 +0000

    Updated Dockerfile and controller

--- src/main/java/com/alexquazar/bmspringjokesapp/controllers/JokeController.java
+++ src/main/java/com/alexquazar/bmspringjokesapp/controllers/JokeController.java
@@ -14,7 +14,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/",""})
+    @RequestMapping({"/","","/jokes"})
     public String showJoke(Model model){
 
         model.addAttribute("joke", jokeService.getJoke());
